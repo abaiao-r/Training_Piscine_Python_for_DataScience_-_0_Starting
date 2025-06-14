65afc6e6be54be41560764388e849d7e0db40e7f
abaiao-r <abaiao-r@UbuntuVM.myguest.virtualbox.org>
2025-06-14 16:07:13 +0100

    ex02 is done

--- ex02/tester.py
+++ ex02/tester.py
@@ -1,24 +1,26 @@
 # **************************************************************************** #
 #                                                                              #
 #                                                         :::      ::::::::    #
-#    format_ft_time.py                                  :+:      :+:    :+:    #
+#    tester.py                                          :+:      :+:    :+:    #
 #                                                     +:+ +:+         +:+      #
 #    By: abaiao-r <abaiao-r@student.42.fr>          +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
-#    Created: 2025/06/14 15:33:25 by abaiao-r          #+#    #+#              #
-#    Updated: 2025/06/14 15:44:32 by abaiao-r         ###   ########.fr        #
+#    Created: 2025/06/14 15:55:53 by abaiao-r          #+#    #+#              #
+#    Updated: 2025/06/14 15:57:38 by abaiao-r         ###   ########.fr        #
 #                                                                              #
 # **************************************************************************** #
 
-import time
-from datetime import datetime 
+from find_ft_type import all_thing_is_obj
 
-# Get current timestamp
-current_time = time.time()
+ft_list = ["Hello", "tata!"]
+ft_tuple = ("Hello", "toto!")
+ft_set = {"Hello", "tutu!"}
+ft_dict = {"Hello": "titi!"}
 
-# Print seconds since epoch in both float and scientific notation
-print(f"Seconds since January 1, 1970: {current_time:,.4f} or {current_time:.2e} in scientific notation")
-
-# Get current date and format as 'Mon DD YYYY'
-now = datetime.now()
-print(now.strftime("%b %d %Y"))
+all_thing_is_obj(ft_list)
+all_thing_is_obj(ft_tuple)
+all_thing_is_obj(ft_set)
+all_thing_is_obj(ft_dict)
+all_thing_is_obj("Brian")
+all_thing_is_obj("Toto")
+print(all_thing_is_obj(10))
